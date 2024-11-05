8b94356b1ca543c0e82cef52934b6308aa565bf8
imanolih <imanolihuss@gmail.com>
2024-11-05 17:18:28 -0300

    Update ejemplo_titanic.py

--- ejemplo_titanic.py
+++ ejemplo_titanic.py
@@ -8,11 +8,12 @@ with st.container():
     st.title("Página con Imagen de Portada")
     st.write("Este es un ejemplo de cómo colocar una imagen en la parte superior que ocupe toda la anchura de la página.")
 
-with st.container(""):
+with st.container():
     st.sidebar.title("mi titulo lateral")
     st.sidebar.image('2021_Facebook_icon.svg.png')
     user_input = st.sidebar.text_input("Email")
     user_input = st.sidebar.text_input("Contraseña")
+    
 
     st.sidebar.write("[terminos y condiciones](https://www.facebook.com/policies_center/)")
     
