b2f5e711f456da6a823cce8e8dc345d76c8518c9
imanolih <imanolihuss@gmail.com>
2024-11-05 15:38:51 -0300

    Update ejemplo_titanic.py

--- ejemplo_titanic.py
+++ ejemplo_titanic.py
@@ -6,3 +6,4 @@ st.title("mi pagina web")
 # Usamos HTML para alinear el título a la izquierda
 st.markdown('<h1 style="text-align: left;">Mi Título a la Izquierda</h1>', unsafe_allow_html=True)
 st.sidebar.title("mi titulo lateral")
+st.sidebar.image('2021_Facebook_icon.svg.png')
