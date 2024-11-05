ff17f4e03d6da4d3693b2c8e8767040c8857feaa
imanolih <imanolihuss@gmail.com>
2024-11-05 15:32:58 -0300

    Update ejemplo_titanic.py

--- ejemplo_titanic.py
+++ ejemplo_titanic.py
@@ -5,4 +5,4 @@ import matplotlib.pyplot as plt
 st.title("mi pagina web")
 # Usamos HTML para alinear el título a la izquierda
 st.markdown('<h1 style="text-align: left;">Mi Título a la Izquierda</h1>', unsafe_allow_html=True)
-st.sidebar.title(mi titulo lateral)
+st.sidebar.title("mi titulo lateral")
