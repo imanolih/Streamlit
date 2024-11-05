3650c1794e9ac53aaf5afff68b0f7de98fb7ca1b
imanolih <imanolihuss@gmail.com>
2024-11-05 16:51:50 -0300

    Update ejemplo_titanic.py

--- ejemplo_titanic.py
+++ ejemplo_titanic.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 
 with st.container():
     imagen = "images.png"
-    st.image(imagen, width=400, height=300)
+    st.image(imagen, use_column_width=True)
     st.title("Página con Imagen de Portada")
     st.write("Este es un ejemplo de cómo colocar una imagen en la parte superior que ocupe toda la anchura de la página.")
 
