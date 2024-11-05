ef0f60167811ee0497c0cbd47876df0a13ed8ae1
imanolih <imanolihuss@gmail.com>
2024-11-05 16:43:00 -0300

    Update ejemplo_titanic.py

--- ejemplo_titanic.py
+++ ejemplo_titanic.py
@@ -3,10 +3,13 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 with st.container():
-  st.markdown("""
+import streamlit as st
+
+# Inyectar CSS para colocar la imagen de portada en la parte superior
+st.markdown("""
     <style>
         .header {
-            background-image: url('images.png'); /* URL de la imagen */
+            background-image: url(''); /* URL de la imagen */
             background-size: cover; /* La imagen cubrirá toda la zona */
             background-position: center; /* Centrar la imagen */
             height: 300px; /* Altura de la imagen (ajusta según lo que necesites) */
@@ -14,8 +17,14 @@ with st.container():
         }
     </style>
 """, unsafe_allow_html=True)
-  titulo = st.text_input("Escribe el título de la página:", "Título predeterminado")
-  st.title("mi pagina web")
+
+# Crear un contenedor para la imagen de portada
+st.markdown('<div class="header"></div>', unsafe_allow_html=True)
+
+# Agregar más contenido a la página
+st.title("Página con Imagen de Portada")
+st.write("Este es un ejemplo de cómo colocar una imagen en la parte superior que ocupe toda la anchura de la página.")
+
 with st.container():
   st.sidebar.title("mi titulo lateral")
   st.sidebar.image('2021_Facebook_icon.svg.png')
