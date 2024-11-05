5fb138d6b19b6efd563d72926971c602917d395d
imanolih <imanolihuss@gmail.com>
2024-11-05 16:24:22 -0300

    Update ejemplo_titanic.py

--- ejemplo_titanic.py
+++ ejemplo_titanic.py
@@ -7,7 +7,9 @@ with st.container():
 with st.container():
   st.sidebar.title("mi titulo lateral")
   st.sidebar.image('2021_Facebook_icon.svg.png')
-  user_imput = st.sidebar.text_input("Ingrese su Email")
+  user_input = st.sidebar.text_input("Email")
+  user_input = st.sidebar.text_input("Contraseña")
+  
     
   
   
