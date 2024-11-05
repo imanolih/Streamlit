e7e7373f0e822e1f23a93bd8c88a3e9efcc33803
imanolih <imanolihuss@gmail.com>
2024-11-05 16:44:10 -0300

    Update ejemplo_titanic.py

--- ejemplo_titanic.py
+++ ejemplo_titanic.py
@@ -4,25 +4,24 @@ import matplotlib.pyplot as plt
 
 with st.container():
 
-# Inyectar CSS para colocar la imagen de portada en la parte superior
-st.markdown("""
-    <style>
-        .header {
-            background-image: url('images.png'); /* URL de la imagen */
-            background-size: cover; /* La imagen cubrirá toda la zona */
-            background-position: center; /* Centrar la imagen */
-            height: 300px; /* Altura de la imagen (ajusta según lo que necesites) */
-            width: 100%; /* Asegura que la imagen ocupe todo el ancho */
-        }
-    </style>
-""", unsafe_allow_html=True)
-
-# Crear un contenedor para la imagen de portada
-st.markdown('<div class="header"></div>', unsafe_allow_html=True)
-
-# Agregar más contenido a la página
-st.title("Página con Imagen de Portada")
-st.write("Este es un ejemplo de cómo colocar una imagen en la parte superior que ocupe toda la anchura de la página.")
+    st.markdown("""
+        <style>
+            .header {
+                background-image: url('images.png'); /* URL de la imagen */
+                background-size: cover; /* La imagen cubrirá toda la zona */
+                background-position: center; /* Centrar la imagen */
+                height: 300px; /* Altura de la imagen (ajusta según lo que necesites) */
+                width: 100%; /* Asegura que la imagen ocupe todo el ancho */
+            }
+        </style>
+    """, unsafe_allow_html=True)
+    
+    # Crear un contenedor para la imagen de portada
+    st.markdown('<div class="header"></div>', unsafe_allow_html=True)
+    
+    # Agregar más contenido a la página
+    st.title("Página con Imagen de Portada")
+    st.write("Este es un ejemplo de cómo colocar una imagen en la parte superior que ocupe toda la anchura de la página.")
 
 with st.container():
   st.sidebar.title("mi titulo lateral")
