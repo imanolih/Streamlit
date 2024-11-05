f0bacf87ebd7032df9908a2d29ebd6f0bb58734c
imanolih <imanolihuss@gmail.com>
2024-11-05 16:40:57 -0300

    Update ejemplo_titanic.py

--- ejemplo_titanic.py
+++ ejemplo_titanic.py
@@ -1,7 +1,22 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+
 with st.container():
+  st.markdown("""
+    <style>
+        .header {
+            background-image: url('images.png'); /* URL de la imagen */
+            background-size: cover; /* La imagen cubrirá toda la zona */
+            background-position: center; /* Centrar la imagen */
+            height: 300px; /* Altura de la imagen (ajusta según lo que necesites) */
+            width: 100%; /* Asegura que la imagen ocupe todo el ancho */
+        }
+    </style>
+""", unsafe_allow_html=True)
+
+# Crear un contenedor para la imagen de portada
+st.markdown('<div class="header"></div>', unsafe_allow_html=True)
   titulo = st.text_input("Escribe el título de la página:", "Título predeterminado")
   st.title("mi pagina web")
 with st.container():
