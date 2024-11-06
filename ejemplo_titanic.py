5735268e6305f529d6a769468ae786f0bd8e875c
imanolih <imanolihuss@gmail.com>
2024-11-06 11:32:53 -0300

    Update ejemplo_titanic.py

--- ejemplo_titanic.py
+++ ejemplo_titanic.py
@@ -7,15 +7,19 @@ with st.container():
     st.subheader("Podras dirigirte a las paginas mas utilizadas")
 with st.container():
     st.write("---")
-    col1, col2, col3 = st.columns(3)
+    col1, col2, col3, col4 = st.columns(4)
 with col1:
     st.image("youtube-logo-youtube-icon-transparent-free-png.webp")
     st.write("YOUTUBE")
 with col2:
     st.image("whatsapp-logo-whatsapp-icon-whatsapp-transparent-free-png.webp")
-    st.write("FACEBOOK")
+    st.write("WHATSAPP")
 with col3:
     st.image("facebook-logo-facebook-icon-transparent-free-png.webp")
+    st.write("FACEBOOK")
+with col4:
+    st.image("")
+    st.write("GITHUB")
       
   
     
