4e2d85f3f199279b176535e67fb5a40b54857ba5
imanolih <imanolihuss@gmail.com>
2024-11-06 11:13:00 -0300

    Update ejemplo_titanic.py

--- ejemplo_titanic.py
+++ ejemplo_titanic.py
@@ -5,6 +5,9 @@ import matplotlib.pyplot as plt
 with st.container():
     st.title("REDIRIGIDOR DE PAGINAS WEB")
     st.subheader("Podras dirigirte a las paginas mas utilizadas")
+with st.container():
+    st.write("---")
+    st.colums(1, 6)
     
       
   
